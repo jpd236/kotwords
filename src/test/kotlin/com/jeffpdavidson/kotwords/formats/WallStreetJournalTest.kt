78e04064bb31f2c3f9c7edf7a7b6ae0f5e3d8bec
Jeff Davidson <jpd236@cornell.edu>
2018-08-05 22:55:57 -0700

    Fix broken tests.

--- src/test/kotlin/com/jeffpdavidson/kotwords/formats/WallStreetJournalTest.kt
+++ src/test/kotlin/com/jeffpdavidson/kotwords/formats/WallStreetJournalTest.kt
@@ -4,16 +4,10 @@ import com.jeffpdavidson.kotwords.readBinaryResource
 import com.jeffpdavidson.kotwords.readUtf8Resource
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import java.nio.file.FileSystems
-import java.nio.file.Files
 
 class WallStreetJournalTest {
     @Test
     fun basicPuzzle() {
-        val path = FileSystems.getDefault().getPath("/Users/Jeff/Desktop/test-nocircles.puz")
-        Files.write(path, WallStreetJournal(WallStreetJournalTest::class.readUtf8Resource("wsj/test-nocircles.json"),
-                includeDateInTitle = false)
-                .asCrossword().toAcrossLiteBinary())
         Assertions.assertArrayEquals(
                 WallStreetJournalTest::class.readBinaryResource("puz/test-nocircles.puz"),
                 WallStreetJournal(WallStreetJournalTest::class.readUtf8Resource("wsj/test-nocircles.json"),
