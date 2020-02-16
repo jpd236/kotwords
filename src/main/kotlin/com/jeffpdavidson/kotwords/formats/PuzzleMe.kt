85b3d1c7128cac6c1a05fa9858fbc60b9c1520b4
Jeff Davidson <jpd236@cornell.edu>
2020-02-15 22:14:33 -0800

    Fix clue corruption in PuzzleMe clue sanitization.
    data.clueNums is column-major, but we were pruning rows/columns as
    though it was row-major.
    
    Fixes The Atlantic puzzle from 2/14, which has a black top row.

--- src/main/kotlin/com/jeffpdavidson/kotwords/formats/PuzzleMe.kt
+++ src/main/kotlin/com/jeffpdavidson/kotwords/formats/PuzzleMe.kt
@@ -92,18 +92,19 @@ class PuzzleMe(private val html: String) : Crosswordable {
                     grid.filter(anyNonBlackSquare).map { row -> row.indexOfFirst { it != BLACK_SQUARE } }.min()!!
             val rightRowsToDelete = grid[0].size -
                     grid.filter(anyNonBlackSquare).map { row -> row.indexOfLast { it != BLACK_SQUARE } }.max()!! - 1
-            fun <T> subGrid(g: List<List<T>>): List<List<T>> = g.drop(topRowsToDelete).dropLast(bottomRowsToDelete)
+            val filteredGrid = grid.drop(topRowsToDelete).dropLast(bottomRowsToDelete)
                     .map { row -> row.drop(leftRowsToDelete).dropLast(rightRowsToDelete) }
-            val filteredGrid = subGrid(grid)
 
             // Sanitize clues since PuzzleMe grids can have non-standard numbering.
             var sanitizedClues =
                     buildClueMap(data.placedWords.filter { it.acrossNotDown }) to
                             buildClueMap(data.placedWords.filter { !it.acrossNotDown })
             if (data.clueNums.isNotEmpty()) {
+                val filteredClueNums = data.clueNums.drop(leftRowsToDelete).dropLast(rightRowsToDelete)
+                        .map { col -> col.drop(topRowsToDelete).dropLast(bottomRowsToDelete) }
                 sanitizedClues = ClueSanitizer.sanitizeClues(
                         filteredGrid,
-                        subGrid(data.clueNums).mapIndexed { x, col ->
+                        filteredClueNums.mapIndexed { x, col ->
                             col.mapIndexed { y, clueNum ->
                                 (x to y) to clueNum
                             }
