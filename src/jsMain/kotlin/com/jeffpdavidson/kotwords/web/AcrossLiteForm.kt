276d3220a349e6d4f867154c7d2d5bbdf2c383b9
Jeff Davidson <jpd236@cornell.edu>
2021-09-24 15:29:00 -0700

    Add PDF support for Across Lite form.

--- src/jsMain/kotlin/com/jeffpdavidson/kotwords/web/AcrossLiteForm.kt
+++ src/jsMain/kotlin/com/jeffpdavidson/kotwords/web/AcrossLiteForm.kt
@@ -2,27 +2,41 @@ package com.jeffpdavidson.kotwords.web
 
 import com.jeffpdavidson.kotwords.KotwordsInternal
 import com.jeffpdavidson.kotwords.formats.AcrossLite
+import com.jeffpdavidson.kotwords.formats.Pdf.asPdf
+import com.jeffpdavidson.kotwords.formats.PdfFonts
 import com.jeffpdavidson.kotwords.js.Interop
 import com.jeffpdavidson.kotwords.model.Puzzle
 import com.jeffpdavidson.kotwords.web.html.FormFields
 import com.jeffpdavidson.kotwords.web.html.Html.renderPage
 
-/** Form to convert Across Lite files into JPZ files. */
+/** Form to convert Across Lite files into other formats. */
 @JsExport
 @KotwordsInternal
 class AcrossLiteForm {
-    private val jpzForm = PuzzleFileForm("across-lite", ::createPuzzle, { getFileName() }, enableSaveData = false)
+    private val form = PuzzleFileForm(
+        "across-lite",
+        ::createPuzzle,
+        { getFileName() },
+        createPdfFn = ::createPdf,
+        enableSaveData = false,
+    )
     private val file: FormFields.FileField = FormFields.FileField("file")
 
     init {
         renderPage {
-            jpzForm.render(this, bodyBlock = {
+            form.render(this, bodyBlock = {
                 file.render(this, "Across Lite (.puz) file")
             })
         }
     }
 
     private suspend fun createPuzzle(): Puzzle = AcrossLite(Interop.readFile(file.getValue())).asPuzzle()
+
+    private suspend fun createPdf(blackSquareLightnessAdjustment: Float): ByteArray =
+        AcrossLite(Interop.readFile(file.getValue())).asPuzzle().asPdf(
+            fontFamily = PdfFonts.getNotoFontFamily(),
+            blackSquareLightnessAdjustment = blackSquareLightnessAdjustment,
+        )
 
     private fun getFileName(): String {
         return file.getValue().name.removeSuffix(".puz")
